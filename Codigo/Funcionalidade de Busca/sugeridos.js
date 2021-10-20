c668187afb453529e441415b8bec1cd63eb2de7a
brunovc81 <68079428+brunovc81@users.noreply.github.com>
2021-10-20 00:03:15 -0300

    Ferramenta de busca
    Busca no index. Banco de dados no JS. Estilo CSS. A medida que digita na barra de busca vai eliminando os resultados.

---
+++ Codigo/Funcionalidade de Busca/sugeridos.js
@@ -0,0 +1,32 @@
+var db = {
+    dados: [
+        {
+            titulo: 'Primeiros Socorros',
+            codigo:'0000008',
+            usuario: 'brunogrupo@gmail.com',
+            descricao: 'Curso dinâmico de linguagem C.',
+            imagem: 'http://lorempixel.com/200/120/food/1',
+            link: 'http://www.nota10.com.br/0000001.html',
+            categoria: 'Saúde',
+            preco: 99.00
+        }, {
+            titulo: 'Culinária Brasileira',
+            codigo:'0000009',
+            usuario: 'bruno@gmail.com',
+            descricao: 'Curso dinâmico de linguagem C++.',
+            imagem: 'http://lorempixel.com/200/120/food/1',
+            link: 'http://www.nota10.com.br/0000002.html',
+            categoria: 'Culinária',
+            preco: 10.00
+        }, {
+            titulo: 'Artesanato',
+            codigo:'00000010',
+            usuario: 'grupo@gmail.com',
+            descricao: 'Curso dinâmico de linguagem PHP.',
+            imagem: 'http://lorempixel.com/200/120/food/1',
+            link: 'http://www.nota10.com.br/0000003.html',
+            categoria: 'Artes',
+            preco: 69.00
+        }
+    ]
+}
