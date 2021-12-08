fe5d17e6ee6dbff3f3f984a9f15c2cda5e4d190e
Paulo <roncarati1209@gmail.com>
2021-12-08 11:33:08 -0300

    Add index.html e curso.html

---
+++ Codigo/Software/js/db_fake.js
@@ -0,0 +1,34 @@
+var db = {
+	"cursos": [
+		{
+			"id": 1,
+			"nome_curso": "Web Moderno",
+			"descricao": "Tudo que vc precisa saber para construir aplicações modernas para Web. Criar aplicações REAIS usando as mais modernas tecnologias do mercado",
+			"autor": "Leonardo Mourao",
+			"aulas": [
+				"Aula A1",
+				"Aula A2",
+				"Aula A3"
+			]
+		},
+		{
+			"id": 2,
+			"nome_curso": "Curso de oratória",
+			"descricao": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam laudantium nemo, debitis ex unde autem harum nihil aliquid voluptas iste odio quas, a ut fuga",
+			"autor": "Luis Fonseca",
+			"aulas": [
+				"Aula B1",
+				"Aula B2",
+				"Aula B3",
+				"Aula B4"
+			]
+		},
+		{
+			"id": 3,
+			"nome_curso": "Programação em C",
+			"descricao": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam laudantium nemo, debitis ex unde autem harum nihil aliquid voluptas iste odio quas, a ut fuga",
+			"autor": "Cléber Machado",
+			"aulas": []
+		}
+	]
+}
